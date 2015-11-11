a869e6fc515772113786250039aa1b78a33cdb7f
Diego Kremer <diego.kremer@gmail.com>
2015-11-11 11:00:30 -0200

    Finished Categoria class and DAO.

--- really-do/classes/Categoria.php
+++ really-do/classes/Categoria.php
@@ -1,8 +1,44 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// declaração de classe
+class Categoria {
+    
+    // declaração de atributos
+    // encapsulamento:
+    // private = apenas na classe
+    // protected = na classe e nas subclasses
+    // public = aberta
+    protected $codigo_cat;
+    protected $nome;
+    protected $inicio;
+    protected $fim;
+    
+    
+    // construtor é definido pela palavra reservada __construct
+    // pode ou não ter parâmetros
+    // para um construtor poder não receber parâmetros deve-se
+    // definir valores padrão como neste exemplo
+    
+    function __construct($codigo_cat, $nome, $inicio, $fim) {
+        $this->codigo_doenca = $codigo_cat;
+        $this->nome = $nome;
+        $this->inicio = $inicio;
+        $this->fim = $fim;
+       
+        
+    }
+    
+    // "método mágico" para criação de set genérico
+    // ou seja, cria um set que pode ser usado por todos os atributos
+    function &__set($prop, $val) {
+        $this->$prop = $val;
+    }
 
+    // "método mágico" para criação de get genérico
+    // ou seja, cria um get que pode ser usado por todos os atributos
+    function &__get($prop) {
+        return $this->$prop;
+    }
+    
+}
+?>
--- really-do/dao/CategoriaDAO.php
+++ really-do/dao/CategoriaDAO.php
@@ -1,8 +1,46 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// incluir a classe para estabelecimento de herança e utilização de método de conexão
+require_once("BancoPDO.php");
 
+// estabelecimento de herança
+class CategoriaDAO extends BancoPDO {
+
+    // no construtor chamada de método de conexão da superclasse e realização de conexão
+    public function __construct() {
+        $this->conexao = BancoPDO::conexao();
+    }
+
+    // método de inserção de dados na base de dados
+    // recebimento de objeto com atributos setados
+    public function inserir($categoria) {
+        try {
+
+            // preparação de statement para execução de SQL na base
+            // casa interrogação corresponde a um valor que será recebido posteriormente
+            $stm = $this->conexao->prepare("INSERT INTO Categoria (codigo_cat, nome, inicio, fim) VALUES (?, ?, ?, ?)");
+
+            echo $estatistica->usuario;
+
+            // passagem de valores na ordem correta de entrada
+            // corresponde a cada ponto de interrogação na SQL do statement
+            $stm->bindValue(1, $estatistica->codigo_cat);
+            $stm->bindValue(2, $estatistica->nome);
+            $stm->bindValue(3, $estatistica->inicio);
+            $stm->bindValue(4, $estatistica->fim);
+            
+
+            // realiza a execução do código na base
+            if ($stm->execute()) {
+                echo "Dados inseridos com sucesso! <br/>";
+                header("Location: ./index.php");
+            }
+
+
+            // tratamento de exceção nativo de PDO
+        } catch (PDOException $e) {
+            echo "Erro: " . $e->getMessage();
+        }
+    }
+
+}
